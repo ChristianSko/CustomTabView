a0ce973bfe691a33cd9431e8a9e969d695661f43
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-08 13:41:42 +0200

    Initial Commit

---
+++ CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by Skorobogatow, Christian on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
